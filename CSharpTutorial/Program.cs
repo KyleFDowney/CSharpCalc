19c2910a241eb6ada4cafdad69492d3323cb2f13
capntechbeard <capntechbeard@gmail.com>
2019-09-19 15:12:54 -0500

    create error handler to prevent division by 0.
    Also changed integers to doubles to allow for better calculations with decimal places.

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -7,7 +7,7 @@ namespace Calculator
         static void Main(string[] args)
         {
             // Declare variables and then initialize to zero.
-            int num1 = 0; int num2 = 0;
+            double num1 = 0; double num2 = 0;
 
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
@@ -15,11 +15,11 @@ namespace Calculator
 
             // Ask the user to type the first number.
             Console.WriteLine("Type a number, and then press Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
+            num1 = Convert.ToDouble(Console.ReadLine());
 
             // Ask the user to type the second number.
             Console.WriteLine("Type another number, and then press Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
 
             // Ask the user to choose an option.
             Console.WriteLine("Choose an option from the following list:");
@@ -42,9 +42,15 @@ namespace Calculator
                     Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
                     break;
                 case "d":
-                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-                    break;
-            }
+                    // Ask the user to enter a non-zero divisor until they do so.
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("Enter a non-zero divisor: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                    }
+             Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+             break;
+     }
             // Wait for the user to respond before closing.
             Console.Write("Press any key to close the Calculator console app...");
             Console.ReadKey();
